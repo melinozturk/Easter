ee187302fa4fc9d5a0bbc82a32cec1e216c30110
melinozturk <MelinOzturk0707@gmail.com>
2025-09-03 21:37:16 +0000

    prints out well but the math is wrong ?

--- Easter.java
+++ Easter.java
@@ -1,10 +1,22 @@
 public class Easter {
 public static void main(String[] args) {
     int y = 2026 ;
-    int a = y % 19 
-    int b = y : 100 
-    int c = y % 100 
-    int d = b:4
+    int a = y % 19 ;
+    int b = y/100 ;
+    int c = y % 100 ;
+    int d = b/4 ;
+    int e = b % 4 ; 
+    int f = (b+8)/25 ; 
+    int g = b - (f+1)/3 ; 
+    int h = (19* a + b - d -g) +15 % 30 ;
+    int i = c/4 ; 
+    int k = c%4 ; 
+    int r = (32 + 2 * e + 2 * i - h - k)%7  ;
+    int m = (a + 11 * h + 22 * r) / 451 ;
+    int n = (h + r- 7) * m + 144 / 31 ;
+    int p = (h + r- 7) * m + 144 % 31 ;
+
+    System.out.println(" In " + y + " Easter fell on " + n + "/" + (p + 1)) ;
 }
 
 
