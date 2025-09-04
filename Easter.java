01f2ba63daf7bdf8718f6b324a553b284c4018cf
melinozturk <MelinOzturk0707@gmail.com>
2025-09-04 15:02:27 +0000

    final

--- Easter.java
+++ Easter.java
@@ -1,22 +1,39 @@
+// Melin Ozturk
+// This program ill calculate the date easter falls on 2026.
+
 public class Easter {
 public static void main(String[] args) {
     int y = 2026 ;
     int a = y % 19 ;
-    int b = y/100 ;
+    System.out.println("a = " + a ) ;
+    int b = y / 100 ;
+    System.out.println("b = " + b ) ;
     int c = y % 100 ;
+    System.out.println("c = " + c ) ;
     int d = b/4 ;
+    System.out.println("d = " + d ) ;
     int e = b % 4 ; 
-    int f = (b+8)/25 ; 
-    int g = (b - f+1)/3 ; 
+    System.out.println("e = " + e ) ;
+    int f = (b+8) / 25 ; 
+    System.out.println("f = " + f ) ;
+    int g = (b - f + 1) / 3 ; 
+    System.out.println("g = " + g ) ;
     int h = (19 * a + b - d - g + 15) % 30 ;
-    int i = c/4 ; 
-    int k = c%4 ; 
-    int r = (32 + 2 * e + 2 * i - h - k)%7  ;
+    System.out.println("h = " + h ) ;
+    int i = c / 4 ; 
+    System.out.println("i = " + i ) ;
+    int k = c % 4 ; 
+    System.out.println("k = " + k ) ;
+    int r = (32 + 2 * e + 2 * i - h - k) % 7 ;
+    System.out.println("r = " + r ) ;
     int m = (a + 11 * h + 22 * r) / 451 ;
-    int n = (h + r- 7) * m + 144 / 31 ;
-    int p = (h + r- 7) * m + 144 % 31 ;
+    System.out.println("m = " + m ) ;
+    int n = (h + r - 7 * m + 114)/ 31 ;
+    System.out.println("n = " + n ) ;
+    int p = (h + r - 7 * m + 114) % 31 ;
+    System.out.println("p = " + p ) ;
 
-    System.out.println(" In " + y + " Easter falls on " + n + "/" + (p + 1)) ;
+    System.out.println(" In " + y + " Easter will fall on " + n + "/" + (p + 1)) ;
 }
 
 
